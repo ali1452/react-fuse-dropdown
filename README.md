10b87b0324a7bb34540920909aee5313ca6b90c1
ali <aliabbas.mkting@gmail.com>
2025-08-20 16:01:56 +0500

    fix: update DropdownInput prop types to allow string IDs; enhance option styling in the select element

--- README.md
+++ README.md
@@ -3,14 +3,14 @@ react-fuse-dropdown
 Tiny React UI package.
 
 ## Install
-```bash
+
 npm i react-fuse-dropdown
 
 ## Props
 
-rarandomString: random string value to find near or exact option in select box
-data: array of object using for dropdown options
-onChange: a callback function to return back value for your logic
+rarandomString: Random string value to find near or exact option in select box
+data: Array of object using for dropdown options
+onChange: A callback function to return back value for your logic
 variant: "primary" | "ghost"
 label: string value for lable 
 
--- src/DropdownInput.tsx
+++ src/DropdownInput.tsx
@@ -6,8 +6,8 @@ export type InputProps = React.ButtonHTMLAttributes<HTMLButtonElement> & {
   variant?: "primary" | "ghost";
   randomString: string;
   label?: string;
-  onChange: (value: string | {name: string, id: number}) => void;
-  data: {name: string, id: number}[];
+  onChange: (value: string | {name: string, id: number | string, [key: string]: unknown;}) => void;
+  data: {name: string, id: number | string, [key: string]: unknown;}[];
 };
 
 
@@ -68,7 +68,7 @@ export const DropdownInput = React.forwardRef<HTMLButtonElement, InputProps>(
             <label>{label}</label>
             <select onChange={handleChange} value={selectedValue} className="bg-white border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
                 {data.map((item) => (
-                    <option key={item.id} value={item.name}>
+                    <option style={{color:'#000'}} key={item.id} value={item.name}>
                     {item.name}
                     </option>
                 ))}
