3f3ae67fca0972b4f51266bb355d5c48a1957ab1
ali <aliabbas.mkting@gmail.com>
2025-08-20 13:15:52 +0500

    refactor: update package name and README for consistency; enhance dropdown option styling

--- README.md
+++ README.md
@@ -1,7 +1,21 @@
-# @your-scope/react-fuse-dropdown
+react-fuse-dropdown
 
 Tiny React UI package.
 
 ## Install
 ```bash
-npm i @your-scope/react-fuse-dropdown
+npm i react-fuse-dropdown
+
+## Props
+
+rarandomString: random string value to find near or exact option in select box
+data: array of object using for dropdown options
+onChange: a callback function to return back value for your logic
+
+
+
+## Usage
+
+import { DropdownInput } from "react-fuse-dropdown";
+
+
--- src/DropdownInput.tsx
+++ src/DropdownInput.tsx
@@ -61,7 +61,7 @@ export const DropdownInput = React.forwardRef<HTMLButtonElement, InputProps>(
             <label>{label}</label>
             <select onChange={handleChange} value={selectedValue} className="bg-white border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
                 {data.map((item) => (
-                    <option key={item.id} value={item.name}>
+                    <option className="text-black" key={item.id} value={item.name}>
                     {item.name}
                     </option>
                 ))}
